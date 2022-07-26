0f159ce9956bf012abd536998a4933477c20af09
Sunwoo Lee <slee2021@fau.edu>
2022-07-26 14:55:39 -0400

    ColorChanging Code

---
+++ gitDemo.js
@@ -0,0 +1,15 @@
+function randomRGB() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return `rgb(${r}, ${g}, ${b})`;
+}
+
+const letters =
+  document.querySelectorAll(".letter");
+
+setInterval(function () {
+  for (let letter of letters) {
+    letter.style.color = randomRGB();
+  }
+}, 1000);
