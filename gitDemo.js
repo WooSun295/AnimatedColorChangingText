1e75098f68f03fe08a2565ff0959189b00059e98
Sunwoo Lee <slee2021@fau.edu>
2022-07-28 12:16:41 -0400

    GITHUB texts

--- gitDemo.js
+++ gitDemo.js
@@ -1,7 +1,7 @@
 function randomRGB() {
   const r = Math.floor(Math.random() * 256);
   const g = Math.floor(Math.random() * 256);
-  const b = 0;
+  const b = Math.floor(Math.random() * 256);
   return `rgb(${r}, ${g}, ${b})`;
 }
 
